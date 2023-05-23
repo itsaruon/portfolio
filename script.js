437f64b50db60bee27db091cd7bcf382d978e36e
Noura <aoura.nlaboudi@gmail.com>
2023-05-22 20:29:08 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Wait for the document to be fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the about section element
+    const aboutSection = document.getElementById("about");
+  
+    // Add a CSS class to trigger the fade-in animation
+    aboutSection.classList.add("fade-in");
+  });
+  
