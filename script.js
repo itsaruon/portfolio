a994d826c601cf8099b9bad72aae6f2bebe5c6b2
Noura <aoura.nlaboudi@gmail.com>
2023-05-23 22:36:00 -0400

    Centered Title and Description

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // Get the title element in the hero section
-  const heroTitle = document.querySelector("#hero .hero-title");
-
-  // CSS class to trigger the fade-in animation
-  heroTitle.classList.add("fade-in");
-});
