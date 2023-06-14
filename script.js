ba0214f533b727907332805a1f1ae0c3c1fca49c
Noura <aoura.nlaboudi@gmail.com>
2023-06-14 00:26:56 -0400

    Recent updaes

--- script.js
+++ script.js
@@ -16,4 +16,25 @@
   hiddenElements.forEach((el) => observer.observe(el));
 
 
-  
+  const codeAnimation = document.querySelector('.code-animation');
+
+// Generate random 1s and 0s
+function generateRandomCode(length) {
+  let code = '';
+  const characters = '01';
+
+  for (let i = 0; i < length; i++) {
+    code += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+
+  return code;
+}
+
+// Update the code animation periodically
+setInterval(() => {
+  const newCode = generateRandomCode(codeAnimation.children.length);
+  for (let i = 0; i < codeAnimation.children.length; i++) {
+    codeAnimation.children[i].textContent = newCode.charAt(i);
+  }
+}, 200);
+
