9ba6755b6910466fbbd398c1fad01dd6e3efe1f4
Noura <aoura.nlaboudi@gmail.com>
2023-06-18 23:00:21 -0400

    Loading screen

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-<script>
-  AOS.init();
-</script>
-  
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('show');
-      } else {
-        entry.target.classList.remove('show');
-      }
-    });
+AOS.init();
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
   });
+});
 
-  const hiddenElements = document.querySelectorAll('.fade-in');
-  hiddenElements.forEach((el) => observer.observe(el));
+const hiddenElements = document.querySelectorAll('.fade-in');
+hiddenElements.forEach((el) => observer.observe(el));
 
-
-  const codeAnimation = document.querySelector('.code-animation');
+const codeAnimation = document.querySelector('.code-animation');
 
 // Generate random 1s and 0s
 function generateRandomCode(length) {
@@ -37,4 +34,36 @@ setInterval(() => {
     codeAnimation.children[i].textContent = newCode.charAt(i);
   }
 }, 200);
+
+// Animate the loading screen and hide it after the animation completes
+window.addEventListener('load', function() {
+  var loadingScreen = document.getElementById('loading-screen');
+  var loadingProgress = document.getElementById('loading-progress');
+
+  var progressSteps = [0, 15, 25, 35, 55, 70, 76, 88, 100]; // Define the progress steps
+
+  var currentStep = 0;
+  var updateInterval = 175; // Time in milliseconds between each progress update
+
+  var updateProgress = function() {
+    if (currentStep >= progressSteps.length) {
+      loadingProgress.textContent = '100%';
+      gsap.to(loadingScreen, {
+        opacity: 0,
+        pointerEvents: 'none',
+        duration: 0.5,
+        onComplete: function() {
+          loadingScreen.remove();
+        }
+      });
+      return;
+    }
+
+    loadingProgress.textContent = progressSteps[currentStep] + '%';
+    currentStep++;
+    setTimeout(updateProgress, updateInterval);
+  };
+
+  updateProgress();
+});
 
